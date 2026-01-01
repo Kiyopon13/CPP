0291dd605dd59851aa2c9192980c9ae42d1a71e3
Kiyopon <srikhartetali.reddy@gmail.com>
2026-01-01 13:12:40 +0530

    Refactor class name from cse6 to cse

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,39 +1,21 @@
 #include<iostream>
 using namespace std;
-class cse6{
+class cse{
 	int i;
 	public: void seti(int n){i=n;}
 	     int geti(){return i;}
 };
-cse6 newfun()
+cse newfun()
 {
-	cse6 x;
+	cse x;
 	x.seti(10);
 	return x;
 }
 int main()
 {
-	cse6 obj;
-	obj=newfun();
-	cout<<"now we are in main..... printing i="<<obj.geti()<<endl;
-	return 0;
-}#include<iostream>
-using namespace std;
-class cse6{
-	int i;
-	public: void seti(int n){i=n;}
-	     int geti(){return i;}
-};
-cse6 newfun()
-{
-	cse6 x;
-	x.seti(10);
-	return x;
-}
-int main()
-{
-	cse6 obj;
+	cse obj;
 	obj=newfun();
 	cout<<"now we are in main..... printing i="<<obj.geti()<<endl;
 	return 0;
 }
+
